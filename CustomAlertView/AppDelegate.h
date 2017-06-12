f56f5704e6b9156f967f3bcc9bf719046881b02b
gaochao <974140080@qq.com>
2017-06-12 09:10:59 +0800

    Initial Commit

---
+++ CustomAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomAlertView
+//
+//  Created by GaoChao on 2017/6/12.
+//  Copyright © 2017年 GaoChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAlertView
+//
+//  Created by GaoChao on 2017/6/12.
+//  Copyright © 2017年 GaoChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
